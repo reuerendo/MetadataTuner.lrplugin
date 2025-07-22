1b466ae992dafe15fc632f0dcc16a9ec6d362234
Oleh Tiuriakov <152391484+reuerendo@users.noreply.github.com>
2025-07-22 11:26:26 +0200

    Update info.lua
    Settings saving

--- MetadataTuner.lrplugin/info.lua
+++ MetadataTuner.lrplugin/info.lua
@@ -2,13 +2,19 @@ return {
 	LrSdkVersion = 9.0,
 	LrSdkMinimumVersion = 9.0,
 	
-	LrToolkitIdentifier = 'com.metadata.ascii.converter',
+	LrToolkitIdentifier = 'com.metadata.tuner',
 	LrPluginName = LOC "$$$/MetadataAscii/PluginName=Metadata Tuner",
 	
 	LrExportFilterProvider = {
 		title = LOC "$$$/MetadataAscii/ExportFilterProvider/Title=Metadata Tuner",
 		file = 'ExportFilterProvider.lua',
 		id = 'MetadataTuner',
+		exportPresetFields = {
+			{ key = 'enableTemplateProcessing', default = false },
+			{ key = 'titleTemplate', default = "" },
+			{ key = 'captionTemplate', default = "" },
+			{ key = 'enableAsciiConversion', default = true },
+		},
 	},
 	
 	LrPluginInfoProvider = 'PluginInfoProvider.lua',
